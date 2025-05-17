f08ef93c8392b30cf652dea6d474bcbd99dd213e
Aligheri <ivanyevsieiev@gmail.com>
2025-05-17 17:52:03 +0300

    feat: created a scheduler that deletes all revoked tokens. This was made to free space in db after holding revoked tokens to validate and prevent malicious requests

--- jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/jwt/TokenRevoker.java
+++ jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/jwt/TokenRevoker.java
@@ -1,9 +1,16 @@
 package com.yevsieiev.authstarter.jwt;
+
 import com.yevsieiev.authstarter.entity.RevokedToken;
 import com.yevsieiev.authstarter.repository.RevokedTokenRepository;
+
 import java.security.MessageDigest;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Base64;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Service for revoking and checking revoked JWT tokens.
@@ -12,11 +19,14 @@ import java.util.Base64;
 public class TokenRevoker {
     private final RevokedTokenRepository revokedTokenRepository;
     private final TokenCipher tokenCipher;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public TokenRevoker(RevokedTokenRepository revokedTokenRepository, TokenCipher tokenCipher) {
         this.revokedTokenRepository = revokedTokenRepository;
         this.tokenCipher = tokenCipher;
+        scheduleRevokedTokensDeletion();
     }
+
     /**
      * Checks if a token is revoked.
      *
@@ -59,5 +69,25 @@ public class TokenRevoker {
             revokedToken.setRevocationDate(Instant.now());
             revokedTokenRepository.save(revokedToken);
         }
+    }
+
+    private void scheduleRevokedTokensDeletion() {
+        long initialDelay = LocalDateTime.now().until(
+                LocalDateTime.now().plusDays(30).withHour(3).withMinute(0),
+                ChronoUnit.MINUTES
+        );
+
+        long periodMinutes = 30 * 24 * 60;
+
+        scheduler.scheduleAtFixedRate(
+                this::deleteAllRevokedTokensFromDb,
+                initialDelay,
+                periodMinutes,
+                TimeUnit.MINUTES
+        );
+    }
+
+    private void deleteAllRevokedTokensFromDb() {
+        revokedTokenRepository.deleteAll();
     }
 }
