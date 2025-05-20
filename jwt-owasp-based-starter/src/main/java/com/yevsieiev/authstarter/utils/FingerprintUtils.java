fa94106acdeee89169273c342a57d4787a823b12
Aligheri <ivanyevsieiev@gmail.com>
2025-05-20 16:37:42 +0300

    refactor: refactored FingerprintUtils.java

--- jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/utils/FingerprintUtils.java
+++ jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/utils/FingerprintUtils.java
@@ -1,6 +1,11 @@
 package com.yevsieiev.authstarter.utils;
 
 import com.yevsieiev.authstarter.exceptions.HashFingerprintException;
+import com.yevsieiev.authstarter.exceptions.InvalidFingerprintException;
+import com.yevsieiev.authstarter.exceptions.MissingFingerprintException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Hex;
 
 import java.nio.charset.StandardCharsets;
@@ -9,9 +14,14 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.HexFormat;
 
+@RequiredArgsConstructor
+@Slf4j
 public class FingerprintUtils {
 
     private static final int FINGERPRINT_LENGTH = 100;
+    private static final String FINGERPRINT_PATTERN = "^[a-zA-Z0-9]{100}$";
+    private static final String HASH_ALGORITHM = "SHA-256";
+    private final CookieUtils cookieUtils;
 
     public static String generateFingerprint() {
         SecureRandom secureRandom = new SecureRandom();
@@ -20,14 +30,29 @@ public class FingerprintUtils {
         return Hex.encodeHexString(randomBytes);
     }
 
-    public static String hashFingerprint(String fingerprint)  {
-        MessageDigest digest = null;
+    public static String hashFingerprint(String fingerprint) {
         try {
-            digest = MessageDigest.getInstance("SHA-256");
+            MessageDigest digest = MessageDigest.getInstance(HASH_ALGORITHM);
+            byte[] hashBytes = digest.digest(fingerprint.getBytes(StandardCharsets.UTF_8));
+            return HexFormat.of().formatHex(hashBytes);
         } catch (NoSuchAlgorithmException e) {
-            throw new HashFingerprintException(e);
+            throw new SecurityException("Critical security component missing", e);
         }
-        byte[] hashBytes = digest.digest(fingerprint.getBytes(StandardCharsets.UTF_8));
-        return HexFormat.of().formatHex(hashBytes);
+    }
+
+    public String validateFingerprint(HttpServletRequest request) {
+        String fingerprint = cookieUtils.extractFingerprintCookie(request);
+
+        if (fingerprint == null) {
+            log.warn("Missing fingerprint cookie");
+            throw new MissingFingerprintException("missing cookie in request");
+        }
+
+        if (!fingerprint.matches(FINGERPRINT_PATTERN)) {
+            log.warn("Invalid fingerprint format");
+            throw new InvalidFingerprintException("invalid fingerprint format");
+        }
+
+        return fingerprint;
     }
 }
