0d6484757e4e06d9d4fcb486cfa4645be26d593b
Aligheri <ivanyevsieiev@gmail.com>
2025-05-21 15:48:13 +0300

    feat : created custom exception for DefaultActivationService

---
+++ jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/exceptions/EmailException.java
@@ -0,0 +1,7 @@
+package com.yevsieiev.authstarter.exceptions;
+
+public class EmailException extends RuntimeException {
+    public EmailException(String message) {
+        super(message);
+    }
+}
