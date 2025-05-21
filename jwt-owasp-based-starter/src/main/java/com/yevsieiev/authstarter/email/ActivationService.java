267659934e36563cb519f7063239e07617002f26
Aligheri <ivanyevsieiev@gmail.com>
2025-05-21 15:44:13 +0300

    feat: created ActivationService.java for flexible implementation

---
+++ jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/email/ActivationService.java
@@ -0,0 +1,7 @@
+package com.yevsieiev.authstarter.email;
+
+public interface ActivationService {
+    String generateActivationCode(String identifier);
+    boolean validateActivationCode(String identifier, String code);
+    void sendActivationEmail(String email, String code);
+}
