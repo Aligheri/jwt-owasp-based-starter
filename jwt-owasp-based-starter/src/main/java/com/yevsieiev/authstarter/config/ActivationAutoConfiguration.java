b2e29b08e4f5d3265b9085b9ce8b896231af1e98
Aligheri <ivanyevsieiev@gmail.com>
2025-05-23 16:57:13 +0300

    refactor : fixed annotation bug

--- jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/config/ActivationAutoConfiguration.java
+++ jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/config/ActivationAutoConfiguration.java
@@ -4,25 +4,29 @@ import com.yevsieiev.authstarter.email.ActivationService;
 import com.yevsieiev.authstarter.email.DefaultActivationService;
 import com.yevsieiev.authstarter.email.EmailConfig;
 import com.yevsieiev.authstarter.exceptions.EmailException;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
-@Configuration
-@EnableConfigurationProperties(EmailConfig.class)
+
+@AutoConfiguration
+@EnableConfigurationProperties({EmailConfig.class})
+@ConditionalOnClass({EnableWebSecurity.class})
 public class ActivationAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnProperty(name = "app.email.enabled", havingValue = "true")
     public JavaMailSender javaMailSender(EmailConfig emailConfig) {
         return emailConfig.mailSender();
     }
