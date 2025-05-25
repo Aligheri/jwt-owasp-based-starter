8244f4e00efd8daa3dc93aaa9da8e41fa5e525d4
Aligheri <ivanyevsieiev@gmail.com>
2025-05-25 14:31:04 +0300

    BIRTHDAY commit !

--- jwt-owasp-based-starter/src/test/java/com/yevsieiev/authstarter/email/EmailConfigTest.java
+++ jwt-owasp-based-starter/src/test/java/com/yevsieiev/authstarter/email/EmailConfigTest.java
@@ -1,11 +1,8 @@
 package com.yevsieiev.authstarter.email;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSender;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class EmailConfigTest {
 
