892185e71db54565c7ed42d5b258dc40fa1a0ac7
Aligheri <ivanyevsieiev@gmail.com>
2025-05-26 17:24:33 +0300

    refactor : deleted unnecessary import

--- jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/dto/response/register/RegisterResponse.java
+++ jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/dto/response/register/RegisterResponse.java
@@ -1,7 +1,5 @@
 package com.yevsieiev.authstarter.dto.response.register;
 
-import java.util.UUID;
-
 public interface RegisterResponse {
     String getMessage();
     void setMessage(String message);
