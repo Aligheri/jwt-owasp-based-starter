eae2097e7dd838c7684ce0f8d71a9325dd7b3946
Aligheri <ivanyevsieiev@gmail.com>
2025-05-27 17:57:33 +0300

    refactor: created new constructor for exception

--- jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/exceptions/TokenDecryptionException.java
+++ jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/exceptions/TokenDecryptionException.java
@@ -1,7 +1,10 @@
 package com.yevsieiev.authstarter.exceptions;
 
-public class TokenDecryptionException extends RuntimeException {
+public  class TokenDecryptionException extends RuntimeException {
     public TokenDecryptionException(String message, Throwable cause) {
         super(message, cause);
+    }
+    public TokenDecryptionException(String message) {
+        super(message);
     }
 }
