290150458fcd2cf964764eee792f98796eea124a
Aligheri <ivanyevsieiev@gmail.com>
2025-05-27 17:56:39 +0300

    feat: created new exception

---
+++ jwt-owasp-based-starter/src/main/java/com/yevsieiev/authstarter/exceptions/SecurityConfigurationException.java
@@ -0,0 +1,7 @@
+package com.yevsieiev.authstarter.exceptions;
+
+public class SecurityConfigurationException extends RuntimeException {
+    public SecurityConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
